a30e8e8168591f0f2aee8cf09e25a248e896eaa8
Sreeram Jagannath <ssreeramj@gmail.com>
2024-07-14 05:15:18 +0000

    updated api url

--- test_api.py
+++ test_api.py
@@ -2,15 +2,15 @@ import json
 
 import requests
 
-response = requests.post(
-    "http://localhost:8000/generate-blog/invoke",
-    json={"input": "What is your name? and what is the weather like in Mumbai"},
-)
-
 # response = requests.post(
-#     "https://orange-chainsaw-9jqqqx6jx4v3xr7w-8000.app.github.dev/generate-blog/invoke",
-#     json={"input": "what is the weather like in mumbai?"},
+#     "http://localhost:8000/generate-blog/invoke",
+#     json={"input": "What is your name? and what is the weather like in Mumbai"},
 # )
+
+response = requests.post(
+    "https://pretty-wanda-ssreeramj-0048b972.koyeb.app/generate-blog/invoke",
+    json={"input": "What are you doing?"},
+)
 
 # print(response)
 
