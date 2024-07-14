f7f8292cd02ed64ea28a00853ab3267830633dad
Sreeram Jagannath <ssreeramj@gmail.com>
2024-07-14 11:08:19 +0000

    added blog agent

--- test_api.py
+++ test_api.py
@@ -2,15 +2,15 @@ import json
 
 import requests
 
-# response = requests.post(
-#     "http://localhost:8000/generate-blog/invoke",
-#     json={"input": "What is your name? and what is the weather like in Mumbai"},
-# )
-
 response = requests.post(
-    "https://pretty-wanda-ssreeramj-0048b972.koyeb.app/generate-blog/invoke",
-    json={"input": "What are you doing?"},
+    "http://localhost:8000/generate-blog/invoke",
+    json={"input": "https://chatgpt.com/share/3a3d6737-2a4d-45b9-9d0d-9cd88fdc2fd2"},
 )
+
+# response = requests.post(
+#     "https://pretty-wanda-ssreeramj-0048b972.koyeb.app/generate-blog/invoke",
+#     json={"input": "What is the weather in Chennai? Do I need to wear a sweater?"},
+# )
 
 # print(response)
 
